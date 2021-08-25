0a553de42aa4e2f8823ce54f5b0c867c104f621e
ArisGoi <aris.goi97@gmail.com>
2021-08-25 13:01:00 +0200

    fix js

--- mail/js/custom.js
+++ mail/js/custom.js
@@ -5,14 +5,17 @@ var whiteList = ["samuele@boolean.com", "info@boolean.com", "fabrizio@boolean.co
 var mailControl = prompt('Scrivi la tua email');
 
 // 3) Controllo se l amail inserita corrisponde a quella nella whiteList
+var whitePass = false;
+
 for ( i = 0; i < whiteList.length; i++){
-    // Se corrisponde attivo l'alert positivo
+
     if (mailControl == whiteList[i]){
-        alert('La tua email è nella white-list');
-        break
+        whitePass = true;
     }
 }
 // 4) Se il ciclo precendete non si è realizzato (e non ha attivato il break) allora la var i sarà maggiore di 0 e si attivo l'alert negativo
-if (i > 0){
-    alert('La tua email NON è nella white-list');
+if (whitePass == true){
+    alert('La mail è nella whitelist!');
+} else {
+    alert('La mail non è nella whitelist');
 }
